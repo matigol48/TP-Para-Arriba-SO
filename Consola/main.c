c28491a379a8b27118ab94e6396ae7c02856d37f
utnso <ari.habib.arg@gmail.com>
2023-04-09 21:58:35 +0000

    meti includes en el main de console

---
+++ Consola/main.c
@@ -0,0 +1,11 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+
+#include "utils.h"
