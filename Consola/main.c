5b4fc4d4376c32e67a7fb4906fe01e2785b9543e
Alejo-Gurfein <agurfein@frba.utn.edu.ar>
2023-04-10 21:59:57 +0000

    conexion kernel-consola v1

--- Consola/main.c
+++ Consola/main.c
@@ -4,4 +4,28 @@
 #include<commons/string.h>
 #include<commons/config.h>
 #include<readline/readline.h>
+#include "utils.c"
 
+int main(void)
+{
+
+
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+	char* path = "/home/utnso/tp0/tp0.log";
+	char* nombre_proceso = "log_tp0";
+	char* valor_consola;
+
+
+	t_log* logger;
+	t_config* config;
+
+
+	logger = iniciar_logger();
+	logger = log_create(path, nombre_proceso, true, LOG_LEVEL_INFO);
+
+    conexion = crear_conexion(ip, puerto);
+    terminar_programa(conexion, logger, config);
+    }
--- Kernel/main.c
+++ Kernel/main.c
@@ -2,4 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <commons/log.h>
+#include "utils.c"
 
+int main(void) {
+	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
+
+	int server_fd = iniciar_servidor();
+	log_info(logger, "Servidor listo para recibir al cliente");
+	int cliente_fd = esperar_cliente(server_fd);
+    }
