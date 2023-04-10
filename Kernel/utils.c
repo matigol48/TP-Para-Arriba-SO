f64c967d0eea1c4d4501b6a4edeceb9a6cf153d0
Ariel-Hab <ari.habib.arg@gmail.com>
2023-04-10 21:43:05 +0000

    termine de agregar utils a consola y kernel

---
+++ Kernel/utils.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<commons/log.h>
+#include<commons/collections/list.h>
+#include<string.h>
+#include<assert.h>
+
+#define PUERTO "4444"
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+extern t_log* logger;
+
+void* recibir_buffer(int*, int);
+
+int iniciar_servidor(void);
+int esperar_cliente(int);
+t_list* recibir_paquete(int);
+void recibir_mensaje(int);
+int recibir_operacion(int);
+
+t_log* logger;
+
+int iniciar_servidor(void)
+{
+	int socket_servidor;
+
+	struct addrinfo hints, *servinfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, "4444", &hints, &servinfo);
+
+	socket_servidor = socket(servinfo->ai_family,
+	                         servinfo->ai_socktype,
+	                         servinfo->ai_protocol);
+
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	listen(socket_servidor, SOMAXCONN);
+
+	freeaddrinfo(servinfo);
+	log_trace(logger, "Listo para escuchar a mi cliente");
+
+	return socket_servidor;
+}
+
+int esperar_cliente(int socket_servidor)
+{
+	// Quitar esta línea cuando hayamos terminado de implementar la funcion
+	//assert(!"no implementado!");
+
+	// Aceptamos un nuevo cliente
+	int socket_cliente;
+	socket_cliente = accept(socket_servidor, NULL, NULL);
+	log_info(logger, "Se conecto un cliente!");
+
+	return socket_cliente;
+}
+
+int recibir_operacion(int socket_cliente)
+{
+	int cod_op;
+	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+		return cod_op;
+	else
+	{
+		close(socket_cliente);
+		return -1;
+	}
+}
+
+void* recibir_buffer(int* size, int socket_cliente)
+{
+	void * buffer;
+
+	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}
+
+void recibir_mensaje(int socket_cliente)
+{
+	int size;
+	char* buffer = recibir_buffer(&size, socket_cliente);
+	log_info(logger, "Me llego el mensaje %s", buffer);
+	free(buffer);
+}
+
+t_list* recibir_paquete(int socket_cliente)
+{
+	int size;
+	int desplazamiento = 0;
+	void * buffer;
+	t_list* valores = list_create();
+	int tamanio;
+
+	buffer = recibir_buffer(&size, socket_cliente);
+	while(desplazamiento < size)
+	{
+		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
+		desplazamiento+=sizeof(int);
+		char* valor = malloc(tamanio);
+		memcpy(valor, buffer+desplazamiento, tamanio);
+		desplazamiento+=tamanio;
+		list_add(valores, valor);
+	}
+	free(buffer);
+	return valores;
+}
